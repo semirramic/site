17b32d83866abec7d4cba27365b31801c5b3eaac
semirramic <semir.ramic61@gmail.com>
2025-05-19 16:39:48 +0200

    changes

--- portfolio/bootstrap/js/custom.js
+++ portfolio/bootstrap/js/custom.js
@@ -1,59 +1,64 @@
-
-    // LOGO IN ANIM
-
 gsap.registerPlugin(ScrollTrigger);
 
-  ScrollTrigger.matchMedia({
+ScrollTrigger.matchMedia({
 
-    // Desktop
-    "(min-width: 768px)": function() {
-      gsap.to(".logo-container", {
-        opacity: 0,
-        scale: 15,
-        transformOrigin: "50% 55%",
-        scrollTrigger: {
-          trigger: ".logo-container",
-          start: "top top",
-          end: () => "+=" + window.innerHeight * 2,
-          scrub: true,
-          pin: true,
-        }
-      });
-    },
+  // Desktop
+  "(min-width: 768px)": function() {
+    gsap.to(".logo-container", {
+      opacity: 0,
+      scale: 15,
+      transformOrigin: "50% 55%",
+      scrollTrigger: {
+        trigger: ".logo-container",
+        start: "top top",
+        endTrigger: ".CARD",
+        end: "bottom bottom",
+        scrub: true,
+        pin: true,
+        markers: true,
+      }
+    });
+  },
 
-    // Mobile
-    "(max-width: 767px)": function() {
-      gsap.to(".logo-container", {
-        opacity: 0,
-        scale: 8,
-        transformOrigin: "50% 55%",
-        scrollTrigger: {
-          trigger: ".logo-container",
-          start: "top top",
-          end: () => "+=" + window.innerHeight * 1.5,
-          scrub: true,
-          pin: true,
-        }
-      });
-    }
+  // Mobile
+  "(max-width: 767px)": function() {
+    gsap.to(".logo-container", {
+      opacity: 0,
+      scale: 8,
+      transformOrigin: "50% 55%",
+      scrollTrigger: {
+        trigger: ".logo-container",
+        start: "top top",
+        endTrigger: ".CARD",
+        end: ".CARD",
+        scrub: true,
+        pin: true,
+        markers: true,
+      }
+    });
+  }
 
-  });
+});
 
-  // Background fade to black for all devices
-  gsap.to("body", {
-    backgroundColor: "#000",
-    color: "#fff",
-    scrollTrigger: {
-      trigger: ".logo-container",
-      start: "bottom top",
-      end: () => "+=" + window.innerHeight,
-      scrub: true,
-    }
-  });
+// Background fade
+gsap.to("body", {
+  backgroundColor: "#000",
+  color: "#fff",
+  scrollTrigger: {
+    trigger: ".logo-container",
+    start: "bottom top",
+    end: () => "+=" + window.innerHeight,
+    scrub: true,
+    markers: true,
+  }
+});
 
-      // LOGO IN ANIM
 
-      
+
+
+
+
+
 
       
 
