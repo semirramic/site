88c5e79d051d6aa74cff2e38bcd77bf0cd00e2cf
semirramic <semir.ramic61@gmail.com>
2025-05-21 17:34:20 +0200

    latest changes

--- portfolio/bootstrap/js/custom.js
+++ portfolio/bootstrap/js/custom.js
@@ -3,11 +3,8 @@ gsap.registerPlugin(ScrollTrigger);
 ScrollTrigger.matchMedia({
 
   // Desktop
-  "(min-width: 768px)": function() {
-    gsap.to(".logo-container", {
-      opacity: 0,
-      scale: 15,
-      transformOrigin: "50% 55%",
+  "(min-width: 768px)": function () {
+    const tl = gsap.timeline({
       scrollTrigger: {
         trigger: ".logo-container",
         start: "top top",
@@ -15,26 +12,54 @@ ScrollTrigger.matchMedia({
         end: "bottom bottom",
         scrub: true,
         pin: true,
-        markers: true,
+        markers: true, // remove in production
       }
+    });
+
+    const desktopSteps = [
+      { scale: 1.5, origin: "50% 51%", opacity: 1 },
+      { scale: 3, origin: "50% 52%", opacity: 1 },
+      { scale: 5, origin: "50% 53%", opacity: 1 },
+      { scale: 7.5, origin: "50% 54%", opacity: 1 },
+      { scale: 11, origin: "50% 55%", opacity: 0 },
+      { scale: 20, origin: "50% 100%", opacity: 0 },
+    ];
+
+    desktopSteps.forEach(step => {
+      tl.to(".logo-container", {
+        scale: step.scale,
+        transformOrigin: step.origin,
+        opacity: step.opacity,
+      });
     });
   },
 
   // Mobile
-  "(max-width: 767px)": function() {
-    gsap.to(".logo-container", {
-      opacity: 0,
-      scale: 8,
-      transformOrigin: "50% 55%",
+  "(max-width: 767px)": function () {
+    const tl = gsap.timeline({
       scrollTrigger: {
         trigger: ".logo-container",
         start: "top top",
         endTrigger: ".CARD",
-        end: ".CARD",
+        end: "bottom bottom",
         scrub: true,
         pin: true,
-        markers: true,
+        markers: true, // remove in production
       }
+    });
+
+    const mobileSteps = [
+      { scale: 1, origin: "50% 55%", opacity: 1 },
+      { scale: 6, origin: "50% 75%", opacity: 1 },
+      { scale: 8, origin: "50% 100%", opacity: 0 },
+    ];
+
+    mobileSteps.forEach(step => {
+      tl.to(".logo-container", {
+        scale: step.scale,
+        transformOrigin: step.origin,
+        opacity: step.opacity,
+      });
     });
   }
 
@@ -52,6 +77,7 @@ gsap.to("body", {
     markers: true,
   }
 });
+
 
 
 
